64e724e9af73d1521c5e0fba00cc53b07e9b3444
Glo Austin <glo.austin19@gmail.com>
2019-01-02 18:40:37 -0800

    fixed error message related to ErrorBoundary

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -61,7 +60,6 @@ class App extends Component {
                 age={person.age}
                 key={person.id}
                 changed={(event) => this.nameChangedHandler(event, person.id)} />
-                
         })}
        </div>
       );
